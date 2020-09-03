48d56a3864ee09ed297fcb38d748bfd73aa3bdc5
parsley1400 <55346686+parsley1400@users.noreply.github.com>
2020-09-03 22:12:20 +0900

    Initial Commit

---
+++ Count/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Count
+//
+//  Created by risako takeya on 2020/09/03.
+//  Copyright © 2020 risako takeya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
